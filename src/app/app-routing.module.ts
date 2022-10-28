3412446dbe23960af65ec18cdf0de0a81e363c5b
Sam Schultz <sam.schultz@outlook.com>
2022-10-28 12:25:33 -0400

    initWithMenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { SupportComponent } from './support/support.component';
 
-const routes: Routes = [];
+const routes: Routes = [            //This is where you put your url for different pages
+{path: "", redirectTo: "/home", pathMatch: "full"},   //The three keys you need to make your route
+{path: "home", component: HomeComponent},
+{path: "about", component: AboutComponent},
+{path: "support", component: SupportComponent}
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
